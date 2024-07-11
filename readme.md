92fe91f13b4014696289b635f1cb8cc3e22e9ff8
Your Name <you@example.com>
2024-07-11 10:15:04 +0530

    add read me

---
+++ readme.md
@@ -0,0 +1,4 @@
+D:\react-native\Pet Mobile\my-app\src\images\screenshots\loading.jpg
+D:\react-native\Pet Mobile\my-app\src\images\screenshots\start.jpg
+D:\react-native\Pet Mobile\my-app\src\images\screenshots\home.jpg
+D:\react-native\Pet Mobile\my-app\src\images\screenshots\describe.jpg
--- src/screen/describePage.tsx
+++ src/screen/describePage.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, StyleSheet, Alert, ScrollView } from 'react-native';
-import Card from '../component/card';
+import DescribeCard from '../component/describeCard';
 
 export default function DescribePage(){
     return(
-           <View>
-            <Card
-                    imageSource={require('../images/dog.png')}
-                    
-                />
-           </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <DescribeCard 
+            imageSource={require('../images/dog.png')}
+            description="This is an example of a card component that includes an image and text. You can customize this content to fit your needs."
+             />
+        </ScrollView>
     )
-}
+};
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      backgroundColor: '#f0f0f0',
+      flexGrow: 1,
+    }
+});
+    
+   
