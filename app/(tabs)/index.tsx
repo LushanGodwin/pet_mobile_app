deecc2889c984d89f6ec8e3a6a43a92d749be183
Your Name <you@example.com>
2024-07-18 09:00:05 +0530

    few changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,45 @@
 import React from 'react';
 import { View, StyleSheet, Alert, ScrollView } from 'react-native';
 import Card from '@/components/card';
+import { useRouter } from 'expo-router';
 
 export default function DashboardPage() {
+    const router = useRouter();
+
     const handleButtonPress = () => {
-        Alert.alert("Button Pressed!");
+        router.push('/details');
     };
 
     return (
         <ScrollView>
 <View style={styles.container}>
                 <Card
-                    imageSource={require('../(home)/images/dog.png')}
+                    imageSource={require('../../assets/images/dog.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
                 <Card
-                    imageSource={require('../(home)/images/dog1.png')}
+                    imageSource={require('../../assets/images/dog1.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
                 <Card
-                    imageSource={require('../(home)/images/dog2.png')}
+                    imageSource={require('../../assets/images/dog2.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
                 <Card
-                    imageSource={require('../(home)/images/dog3.png')}
+                    imageSource={require('../../assets/images/dog3.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
                 <Card
-                    imageSource={require('../(home)/images/dog.png')}
+                    imageSource={require('../../assets/images/dog.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
                 <Card
-                    imageSource={require('../(home)/images/dog.png')}
+                    imageSource={require('../../assets/images/dog.png')}
                     buttonText="Read More"
                     onButtonPress={handleButtonPress}
                 />
